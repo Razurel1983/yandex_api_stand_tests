7aa4382222dd9c5e07130a0f61db7aee93dc3fd5
Semen Elokhin <s.a.elohin@gmail.com>
2023-12-03 16:40:48 +0700

    My first commit

---
+++ data.py
@@ -0,0 +1,8 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+user_body = dict(firstName="Аа", phone="+79995553322", address="г. Москва, ул. Пушкина, д. 10")
+product_ids = {
+    "ids": [1, 2, 3]
+}
---
+++ sender_stand_request.py
@@ -0,0 +1,13 @@
+import configuration
+import requests
+import data
+
+
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  # подставляем полный url
+                         json=body,  # тут тело
+                         headers=data.headers)  # а здесь заголовки
+
+response = post_new_user(data.user_body);
+print(response.status_code)
+print(response.json())
